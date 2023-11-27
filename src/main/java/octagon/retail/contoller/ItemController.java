ad69af13b8dfa371f7753aed05f47e88393e3af7
Tuguldur515 <102710593+Tuguldur515@users.noreply.github.com>
2023-11-27 17:02:53 +0800

    added itemService

---
+++ src/main/java/octagon/retail/contoller/ItemController.java
@@ -0,0 +1,45 @@
+package octagon.retail.contoller;
+
+import octagon.retail.entity.Items;
+import octagon.retail.reponse.ResponseModel;
+import octagon.retail.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @PostMapping("save-item")
+    public ResponseEntity<ResponseModel<Items>> saveItemPrice(@RequestBody Items item){
+        return itemService.saveItem(item);
+    }
+    @PostMapping("update-item")
+    public ResponseEntity<ResponseModel<Items>> getAllItemPrice(@RequestBody Items item){
+        return itemService.updateItem(item);
+    }
+    @GetMapping("get-all-items")
+    public ResponseEntity<ResponseModel<List<Items>>> getAllByItemId() {
+        return itemService.getAllItems();
+    }
+
+    @GetMapping("get-item-by-id")
+    public ResponseEntity<ResponseModel<Items>> getByItemPriceId(@RequestParam("id") Long itemId){
+    return itemService.getItemById(itemId);
+    }
+
+    @GetMapping("get-item-by-code")
+    public ResponseEntity<ResponseModel<Items>> getItemByCode(@RequestParam("code") String code){
+        return itemService.getItemByCode(code);
+    }
+    @DeleteMapping("delete-item-by-id")
+    public ResponseEntity<ResponseModel<Items>> DeleteAllByItemId(@RequestParam("id") Long itemId) {
+        return itemService.deleteItemById(itemId);
+    }
+}
--- src/main/java/octagon/retail/service/ItemService.java
+++ src/main/java/octagon/retail/service/ItemService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ItemService {
 
@@ -20,5 +22,53 @@ public class ItemService {
             return ResponseEntity.ok(new ResponseModel<>("200", "Амжилттай", true, item));
         }
         return ResponseEntity.ok(new ResponseModel<>("404","Амжилтгүй - Өмнө нь бүртгэгдсэн бараа байна!",false,null));
+    }
+    public ResponseEntity<ResponseModel<Items>> updateItem(Items item){
+        Items existingItem = itemRepository.findById(item.getId()).orElse(null);
+        if (existingItem != null){
+            existingItem.setName(item.getName());
+            existingItem.setCode(item.getCode());
+            existingItem.setIsActive(item.getIsActive());
+            existingItem.setMeasureId(item.getMeasureId());
+            existingItem.setIsActive(item.getIsActive());
+            existingItem.setIsDeleted(item.getIsDeleted());
+            existingItem.setBranchId(item.getBranchId());
+            itemRepository.save(existingItem);
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилттай",true,  existingItem));
+        }
+        return ResponseEntity.ok(new ResponseModel<>("404","Амжилтгүй",false,null));
+    }
+    public ResponseEntity<ResponseModel<Items>> getItemById(Long itemId){
+        Items item = itemRepository.findById(itemId).orElse(null);
+        if (item != null){
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилтгүй", true, item));
+        } else {
+            return ResponseEntity.ok(new ResponseModel<>("200","Амжилтгүй",false,null));
+        }
+    }
+    public ResponseEntity<ResponseModel<List<Items>>> getAllItems() {
+        List<Items> items = itemRepository.findAll();
+        if (items != null) {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилттай", true, items));
+        } else {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилтгүй", false, null));
+        }
+    }
+    public ResponseEntity<ResponseModel<Items>> getItemByCode(String code) {
+        Items item = itemRepository.getItemByCode(code);
+        if (item != null) {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилттай", true, item));
+        } else {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилтгүй", false, null));
+        }
+    }
+    public ResponseEntity<ResponseModel<Items>> deleteItemById(Long itemId) {
+        itemRepository.deleteById(itemId);
+        Items item = itemRepository.findById(itemId).orElse(null);
+        if (item == null) {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилттай", true, null));
+        } else {
+            return ResponseEntity.ok(new ResponseModel<>("200", "Амжилтгүй - алдаа гарлаа ахин оролдон уу", false, item));
+        }
     }
 }
