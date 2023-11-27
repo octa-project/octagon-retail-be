23cf330158faf6e77001f739840240024c5d6c88
Tulgga <43769589+tulgga@users.noreply.github.com>
2023-11-27 17:00:16 +0800

    Sync table created

---
+++ src/main/java/octagon/retail/entity/ItemPrices.java
@@ -0,0 +1,46 @@
+package octagon.retail.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "item_prices")
+public class ItemPrices extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "item_id")
+    private Long itemId;
+
+    @Column(name = "item_code_id")
+    private Long itemCodeId;
+
+    @Column(name = "sale_price")
+    private BigDecimal salePrice;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "is_deleted")
+    private Boolean isDeleted;
+
+    @Column(name = "branch_id")
+    private Long branchId;
+
+}
