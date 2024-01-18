047866c6227396bc954b231c6f39ee21c89a659e
Odbayar Batmunkh <odbayar.b@mcs.mn>
2024-01-18 16:50:30 +0800

    zasvar

--- src/main/java/octagon/retail/contoller/ReportController.java
+++ src/main/java/octagon/retail/contoller/ReportController.java
@@ -3,11 +3,21 @@ package octagon.retail.contoller;
 import net.sf.jasperreports.engine.JRException;
 import octagon.retail.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
 import java.text.ParseException;
 import java.util.Map;
+
+import java.nio.file.Path;
 
 @RestController
 @RequestMapping("/report")
@@ -25,6 +35,19 @@ public class ReportController {
     @PostMapping("/report-test")
     public String getSaleReport() throws JRException, FileNotFoundException {
         return reportService.reportTest();
+    }
+
+    @GetMapping("/getfile")
+    public ResponseEntity<Resource> getFileTest() throws MalformedURLException {
+        File file = new File("D:\\Jasper" + "\\saleReport.html");
+        Path filePath = Paths.get(file.getAbsolutePath());;
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + resource.getFilename());
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
     }
 
 
